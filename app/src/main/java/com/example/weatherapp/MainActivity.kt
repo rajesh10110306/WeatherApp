76f2f991a303f695d499ae45c00f257d74293e07
Chodisetti Rajesh <chodisetti.rajesh@zee.com>
2023-09-04 15:31:05 +0530

    Repository pattern

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapp.repository.WeatherRepository
+import com.example.weatherapp.viewmodel.WeatherViewModel
+import com.example.weatherapp.viewmodel.WeatherViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var repository: WeatherRepository
+    private lateinit var factory: WeatherViewModelFactory
+    private lateinit var viewModel: WeatherViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        repository = (application as WeatherApplication).weatherRepository
+        factory = WeatherViewModelFactory(repository)
+        viewModel = ViewModelProvider(this,factory).get(WeatherViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/WeatherApplication.kt
@@ -0,0 +1,23 @@
+package com.example.weatherapp
+
+import android.app.Application
+import com.example.weatherapp.api.RetrofitHelper
+import com.example.weatherapp.api.RetrofitHelper2
+import com.example.weatherapp.db.WeatherDatabase
+import com.example.weatherapp.repository.WeatherRepository
+
+class WeatherApplication: Application() {
+    lateinit var weatherRepository: WeatherRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        initialize()
+    }
+
+    fun initialize(){
+        val api = RetrofitHelper.api
+        val api2 = RetrofitHelper2.api
+        val database = WeatherDatabase(applicationContext)
+        weatherRepository = WeatherRepository(api,api2,database,applicationContext)
+    }
+}
