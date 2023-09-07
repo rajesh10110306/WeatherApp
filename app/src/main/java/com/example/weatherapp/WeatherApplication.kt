e636935b261b5f03b5b94a6ba36863e7ee92b230
Chodisetti Rajesh <chodisetti.rajesh@zee.com>
2023-09-07 23:29:55 +0530

    Added Modules to koin

--- app/src/main/java/com/example/weatherapp/WeatherApplication.kt
+++ app/src/main/java/com/example/weatherapp/WeatherApplication.kt
@@ -1,26 +1,20 @@
 package com.example.weatherapp
 
 import android.app.Application
-import com.example.weatherapp.api.WeatherBuilder
-import com.example.weatherapp.api.PlaceBuilder
-import com.example.weatherapp.db.WeatherDatabase
-import com.example.weatherapp.repository.WeatherRepository
-import com.example.weatherapp.repository.WeatherRepositoryImpl
+import com.example.weatherapp.di.apiModule
+import com.example.weatherapp.di.appModule
+import com.example.weatherapp.di.databaseModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class WeatherApplication: Application() {
-    companion object{
-        lateinit var weatherRepository: WeatherRepository
-    }
-
     override fun onCreate() {
         super.onCreate()
-        initialize()
-    }
-
-    fun initialize(){
-        val weatherApi = WeatherBuilder.api
-        val placeApi = PlaceBuilder.api
-        val weatherDao = WeatherDatabase(applicationContext).getWeatherDao()
-        weatherRepository = WeatherRepositoryImpl(weatherApi,placeApi,weatherDao,applicationContext)
+        startKoin {
+            androidLogger()
+            androidContext(this@WeatherApplication)
+            modules(appModule, apiModule, databaseModule)
+        }
     }
 }
