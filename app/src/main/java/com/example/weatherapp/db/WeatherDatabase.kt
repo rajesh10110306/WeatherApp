42711636d3d1d5bd0cee9902a3a9bf384c5d25f1
Chodisetti Rajesh <chodisetti.rajesh@zee.com>
2023-09-07 23:46:47 +0530

    Removing dependencies builders

--- app/src/main/java/com/example/weatherapp/db/WeatherDatabase.kt
+++ app/src/main/java/com/example/weatherapp/db/WeatherDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.weatherapp.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.weatherapp.data.LocalWeather
 
@@ -10,23 +8,4 @@ import com.example.weatherapp.data.LocalWeather
 abstract class WeatherDatabase: RoomDatabase() {
 
     abstract fun getWeatherDao(): WeatherDao
-
-    companion object {
-        private const val DB_NAME = "weather_database.db"
-        @Volatile
-        private var instance: WeatherDatabase? = null
-        private val LOCK = Any()
-
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
-            instance ?: buildDatabase(context).also {
-                instance = it
-            }
-        }
-
-        private fun buildDatabase(context: Context) = Room.databaseBuilder(
-            context.applicationContext,
-            WeatherDatabase::class.java,
-            DB_NAME
-        ).build()
-    }
 }
