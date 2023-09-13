ada356367e85800b2dbef531287b52677eca31b2
Chodisetti Rajesh <chodisetti.rajesh@zee.com>
2023-09-13 15:25:52 +0530

    City Info Display in UI

--- app/src/main/java/com/example/weatherapp/compose/CitySearchScreen.kt
+++ app/src/main/java/com/example/weatherapp/compose/CitySearchScreen.kt
@@ -1,34 +1,47 @@
 package com.example.weatherapp.compose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.weatherapp.data.LocalLocation
 import com.example.weatherapp.viewmodel.WeatherViewModel
 
 @Composable
 fun CitySearchScreen(navController: NavController,viewModel: WeatherViewModel){
+    val cityInfo by viewModel.place.collectAsState()
     Column(modifier = Modifier
         .fillMaxSize()
         .background(Color.White)
     ) {
         SearchBar{
             viewModel.getCity(it)
+        }
+        cityInfo.data?.let {
+            CityInfo(cityList = it){label,city ->
+                viewModel.getForecastWeather(label,city)
+                navController.navigateUp()
+            }
         }
     }
 }
@@ -50,4 +63,22 @@ private fun SearchBar(fetchCity: (String)->Unit){
             .fillMaxWidth()
             .shadow(5.dp),
     )
+}
+
+@Composable
+private fun CityInfo(cityList: List<LocalLocation>, fetchForecastWeather: (String,String?)->Unit){
+    LazyColumn(
+    ) {
+        items(count = cityList.size) {
+            Text(cityList[it].label,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        fetchForecastWeather(cityList[it].label,cityList[it].city)
+                    }
+                    .padding(horizontal = 30.dp, vertical = 18.dp),
+                fontSize = 18.sp
+            )
+        }
+    }
 }
